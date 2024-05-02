4b01b2ab77cb8546ab5591b8db8fe89b5dee9d19
dlfmalr <sonhyeseung05@gmail.com>
2024-05-02 15:44:59 +0900

    3-13

--- src/main/java/com/mysite/sbb2/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb2/question/QuestionRepository.java
@@ -2,6 +2,7 @@ package com.mysite.sbb2.question;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
@@ -9,4 +10,6 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
     @Override
     Page<Question> findAll(Pageable pageable);
+
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 }
